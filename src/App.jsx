5b4fd45310556f2ffcc0dc5f36bdbed35414503d
ialbintomy <ialbintomy@gmail.com>
2025-03-16 02:07:54 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Solar from './components/solar'
+
+const App = () => {
+  return (
+    <div>
+      <Solar />
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/components/solar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './solar.css'
+
+const solar = () => {
+  return (
+    <>
+    <div className="body">
+    <div className="container">
+        <div className="sun"></div>
+          <div className="earth">
+            <div className="moon">
+
+            </div>
+          </div>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default solar
